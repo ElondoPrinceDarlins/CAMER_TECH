a3907da829abd71ac273ce1362886620b4b63d2c
Fangmbeng26 <preciousprincessefangmbengnaah@gmail.com>
2024-07-13 17:26:56 +0200

    new changes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const concurrently = require('concurrently');
 const commands = [
 //{ command: 'cd CAMER_TECH-MobileEnd && npx ionic serve', name: 'Ionic', prefixColor: 'blue' },
 { command: 'cd CAMER_TECH-webEnd && npx serve .', name: 'HTML5', prefixColor: 'green' }
+//{ command: 'cd CAMER_TECH-MobileEnd && npx ionic serve', name: 'Ionic', prefixColor: 'blue' },
+{ command: 'cd CAMER_TECH-webEnd && npx serve  .', name: 'HTML5', prefixColor: 'green' }
 ];
 
 concurrently(commands, {
