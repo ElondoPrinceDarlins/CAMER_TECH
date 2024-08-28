ffa6711829d3fdf8196e52628a3d38737ed877c5
ElondoPrinceDarlins <darlinsprince@gmail.com>
2024-08-28 07:33:19 +0100

    backend added

--- CAMER_TECH-webEnd/js/authentication/login/login.js
+++ CAMER_TECH-webEnd/js/authentication/login/login.js
@@ -1,1 +1,13 @@
 
+
+  function togglePassword() {
+    var passwordField = document.getElementById("password");
+    var toggleBtn = document.querySelector(".toggle-password");
+    if (passwordField.type === "password") {
+      passwordField.type = "text";
+      toggleBtn.textContent = "Hide";
+    } else {
+      passwordField.type = "password";
+      toggleBtn.textContent = "Show";
+    }
+  }
