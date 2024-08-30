c6488c1c1953f25194527481c91a7704686c2264
noubissie pavel <noubissiepavel@gmail.com>
2024-08-30 01:27:48 +0100

    [feat] - init

--- CAMER_TECH/src/app/app.component.spec.ts
+++ CAMER_TECH/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'CAMERTECH' title`, () => {
+  it(`should have the 'CAMER_TECH' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('CAMERTECH');
+    expect(app.title).toEqual('CAMER_TECH');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CAMERTECH');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CAMER_TECH');
   });
 });
